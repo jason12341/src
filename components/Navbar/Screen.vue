2cdbae5842fc47004ba48ca659197b5da9a47a10
chenchen5976 <38742582+chenchen5976@users.noreply.github.com>
2018-04-28 17:26:42 +0800

    1.1.50
    搜索栏美化

--- components/Navbar/Screen.vue
+++ components/Navbar/Screen.vue
@@ -4,13 +4,13 @@
 			<!--公共的-->
 			<span class="mui-control-item" v-for="(item,index) in tabs" :key="index">
 				<span class="mui-control-item" @click="onWhereType(index)">
-					{{item.title}} <small>▼</small>
+					{{item.title}} <aby-icon type="arrowdown"></aby-icon>
 				</span>
-				<mt-popup v-model="item.isPopup" position="top" popup-transition="popup-fade" :ref="'a'+index">
-					<ul class="mui-table-view">
-					    <li class="mui-table-view-cell" v-for="(li,i) in item.list" :key="i" @click="onSelectWhere(li,index,item.tabid)" :class="item.title===li.text?'li-active':''">{{li.text}}</li>
-					</ul>
-				</mt-popup>
+			<mt-popup v-model="item.isPopup" position="top" popup-transition="popup-fade" :ref="'a'+index">
+				<ul class="mui-table-view">
+					<li class="mui-table-view-cell" v-for="(li,i) in item.list" :key="i" @click="onSelectWhere(li,index,item.tabid)" :class="item.title===li.text?'li-active':''">{{li.text}}</li>
+				</ul>
+			</mt-popup>
 			</span>
 		</div>
 	</div>
@@ -18,72 +18,114 @@
 
 <script>
 	export default {
-		props: ['fixed','type','noSearch'],
+		props: ['fixed', 'type', 'noSearch'],
 		data() {
 			return {
-				tabs:[{tabid:1,title:'综合排序',isPopup:false,list:[{text:'综合排序',value:1},{text:'价格从高到低',value:2},{text:'价格从低到高',value:3},{text:'人气从高到低',value:4}]},],
+				tabs: [{
+					tabid: 1,
+					title: '综合排序',
+					isPopup: false,
+					list: [{
+						text: '综合排序',
+						value: 1
+					}, {
+						text: '价格从高到低',
+						value: 2
+					}, {
+						text: '价格从低到高',
+						value: 3
+					}, {
+						text: '人气从高到低',
+						value: 4
+					}]
+				}, ],
 				popupVisible: false,
 			}
 		},
 		methods: {
 			// 选择条件监听
-			onSelectWhere(selected,key,id){
+			onSelectWhere(selected, key, id) {
 				this.tabs[key].title = selected.text;
 				this.tabs[key].isPopup = false;
 				selected.tabid = id;
-				this.$emit("eventScreen",selected);
+				this.$emit("eventScreen", selected);
 			},
 			// 条件类型点击监听
-			onWhereType(key){
+			onWhereType(key) {
 				this.tabs[key].isPopup = !this.tabs[key].isPopup;
-				if(this.fixed||this.noSearch){
+				if(this.fixed || this.noSearch) {
 					this.$refs.a0[key].$el.classList.add("mint-popup-top-2");
-				}else{
+				} else {
 					this.$refs.a0[key].$el.classList.remove("mint-popup-top-2");
 				}
 			}
 		},
 		mounted() {
-			switch(parseInt(this.type)){
-				case 10:break;
+			switch(parseInt(this.type)) {
+				case 10:
+					break;
 				case 20:
 					// 供应商
-					this.tabs = this.tabs.concat({tabid:2,title:'星级',isPopup:false,list:[]},{title:'品牌',isPopup:false,list:[]});
+					this.tabs = this.tabs.concat({
+						tabid: 2,
+						title: '星级',
+						isPopup: false,
+						list: []
+					}, {
+						title: '品牌',
+						isPopup: false,
+						list: []
+					});
 					break;
 				case 30:
 					// 酒店
-					this.$abyApi.Sys.getDict('hStarlevel','',(res)=>{
+					this.$abyApi.Sys.getDict('hStarlevel', '', (res) => {
 						let dlist = []
-						for(let i=0,len=res.dicList.length;i<len;i++){
+						for(let i = 0, len = res.dicList.length; i < len; i++) {
 							let info = {};
 							info.text = res.dicList[i].dicName;
 							info.value = res.dicList[i].dicValue;
 							dlist.push(info);
 						}
-						this.tabs = this.tabs.concat({tabid:2,title:'星级',isPopup:false,list:dlist});
+						this.tabs = this.tabs.concat({
+							tabid: 2,
+							title: '星级',
+							isPopup: false,
+							list: dlist
+						});
 					});
-					this.$abyApi.Sys.getDict('hBrand','',(res)=>{
+					this.$abyApi.Sys.getDict('hBrand', '', (res) => {
 						let dlist = []
-						for(let i=0,len=res.dicList.length;i<len;i++){
+						for(let i = 0, len = res.dicList.length; i < len; i++) {
 							let info = {};
 							info.text = res.dicList[i].dicName;
 							info.value = res.dicList[i].dicValue;
 							dlist.push(info);
 						}
-						this.tabs = this.tabs.concat({tabid:3,title:'品牌',isPopup:false,list:dlist});
+						this.tabs = this.tabs.concat({
+							tabid: 3,
+							title: '品牌',
+							isPopup: false,
+							list: dlist
+						});
 					});
 					break;
 				case 40:
 					// 景点
-					this.$abyApi.Sys.getDict('sStarlevel','',(res)=>{
+					this.$abyApi.Sys.getDict('sStarlevel', '', (res) => {
 						let dlist = []
-						for(let i=0,len=res.dicList.length;i<len;i++){
+						for(let i = 0, len = res.dicList.length; i < len; i++) {
 							let info = {};
 							info.text = res.dicList[i].dicName;
 							info.value = res.dicList[i].dicValue;
 							dlist.push(info);
 						}
-						this.tabs = this.tabs.concat({tabid:4,title:'景区级别',isPopup:false,list:dlist});
+						this.tabs = this.tabs.concat({
+							tabid: 4,
+							title: '景区级别',
+							isPopup: false,
+							list: dlist
+						});
 					})
 					break;
 				case 99:
@@ -91,11 +133,11 @@
 					break;
 			}
 		},
-		watch: {  
-		    fixed(val){
-		    	this.isFixed = val;
-		    }  
-		}  
+		watch: {
+			fixed(val) {
+				this.isFixed = val;
+			}
+		}
 	}
 </script>
 
@@ -105,16 +147,26 @@
 		border-bottom: 1px solid #F0F0F0;
 		font-size: 14px;
 	}
-	.mint-popup-top{
+	
+	.mint-popup-top {
 		top: 120px;
 	}
-	.mint-popup-top-2{
-		top:80px
+	
+	.mint-popup-top-2 {
+		top: 80px
 	}
-	.mint-popup{
+	
+	.mint-popup {
 		width: 100%;
 	}
-	.li-active{
+	
+	.li-active {
 		color: #2AC845;
+	}
+	
+	.mui-slider-indicator .mui-icon {
+		font-size: 10px;
+		border: none;
+		width: 12px;
 	}
 </style>
